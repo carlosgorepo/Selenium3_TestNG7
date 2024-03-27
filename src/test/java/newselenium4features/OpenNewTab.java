25297dda5b82d91e96c3ae02bd222283d0e5c39d
Carlos Gomez O <carlosgorepo@gmail.com>
2024-03-27 15:51:21 -0500

    Open Tab/Window new Selenium 4 features

---
+++ src/test/java/newselenium4features/OpenNewTab.java
@@ -0,0 +1,37 @@
+package newselenium4features;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WindowType;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import java.time.Duration;
+
+public class OpenNewTab {
+	WebDriver driver;
+	String baseURL;
+
+	@Before
+	public void setUp() {
+		driver = new FirefoxDriver();
+		baseURL = "https://www.letskodeit.com/practice";
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+	}
+
+	@Test
+	public void test1() throws InterruptedException {
+		driver.get(baseURL);
+		Thread.sleep(2000);
+		driver.switchTo().newWindow(WindowType.TAB); // Crear nueva pestaña Selenium 4
+		Thread.sleep(2000);
+	}
+
+	@After
+	public void tearDown() throws InterruptedException {
+		Thread.sleep(2000);
+		driver.quit();
+	}
+}
